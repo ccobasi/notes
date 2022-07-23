0c9c1ee4a30f7e5b97b3be86e703051feea29769
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-07-23 03:56:08 +0100

    add urls,  views, serializer

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+from pyexpat import model
+from rest_framework.serializers import ModelSerializer
+from . models import Note
+
+class NoteSerializer(ModelSerializer):
+    class Meta:
+        model = Note
+        field = '__all__'
